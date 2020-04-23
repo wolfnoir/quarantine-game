8d37ba2d0e255cd0d63b17826667fa637ab4af99
wolfnoir <amber.yang@stonybrook.edu>
2020-04-23 16:49:42 -0400

    Added JSON file of map

---
+++ maps/QuarantineTiles.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="QuarantineTiles" tilewidth="50" tileheight="50" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/bridge.png"/>
+ </tile>
+ <tile id="1">
+  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/business.png"/>
+ </tile>
+ <tile id="2">
+  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/hospital.png"/>
+ </tile>
+ <tile id="3">
+  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/industry.png"/>
+ </tile>
+ <tile id="4">
+  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/lower class.png"/>
+ </tile>
+ <tile id="5">
+  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/park.png"/>
+ </tile>
+ <tile id="6">
+  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/transport.png"/>
+ </tile>
+ <tile id="7">
+  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/upper class.png"/>
+ </tile>
+ <tile id="8">
+  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/water.png"/>
+ </tile>
+ <tile id="9">
+  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/outside map.png"/>
+ </tile>
+</tileset>
