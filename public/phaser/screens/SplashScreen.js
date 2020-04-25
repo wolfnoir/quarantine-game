25368225726571e9629ae3f2126366a177e8b1a4
wolfnoir <amber.yang@stonybrook.edu>
2020-04-25 12:28:36 -0400

    Ahhhh

--- public/phaser/screens/SplashScreen.js
+++ public/phaser/screens/SplashScreen.js
@@ -6,12 +6,15 @@ class SplashScreen extends Phaser.Scene {
 	}
 
 	preload() {
-		this.load.image('background', './assets/background.png');
+		this.load.image('background', '../assets/background.png');
+		this.load.image('titleText', '../assets/quarantine-title-text.png');
 	}
 
 	create() {
 		var bg = this.add.sprite(0,0,'background');
-    	bg.setOrigin(0,0);
+		bg.setOrigin(0,0);
+		
+		var text = this.add.text(100,100, 'Welcome To My Game!');
 	}
 
 }
