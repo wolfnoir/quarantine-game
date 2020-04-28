bc3f427f5e7776f31315f3cabfec200e34e6bacc
wolfnoir <amber.yang@stonybrook.edu>
2020-04-27 22:17:19 -0400

    Placed tile images in the proper location.

--- maps/QuarantineTiles.tsx
+++ maps/QuarantineTiles.tsx
@@ -2,33 +2,33 @@
 <tileset version="1.2" tiledversion="1.3.4" name="QuarantineTiles" tilewidth="50" tileheight="50" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/bridge.png"/>
+  <image width="50" height="50" source="tiles/bridge.png"/>
  </tile>
  <tile id="1">
-  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/business.png"/>
+  <image width="50" height="50" source="tiles/business.png"/>
  </tile>
  <tile id="2">
-  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/hospital.png"/>
+  <image width="50" height="50" source="tiles/hospital.png"/>
  </tile>
  <tile id="3">
-  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/industry.png"/>
+  <image width="50" height="50" source="tiles/industry.png"/>
  </tile>
  <tile id="4">
-  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/lower class.png"/>
+  <image width="50" height="50" source="tiles/lower class.png"/>
  </tile>
  <tile id="5">
-  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/park.png"/>
+  <image width="50" height="50" source="tiles/park.png"/>
  </tile>
  <tile id="6">
-  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/transport.png"/>
+  <image width="50" height="50" source="tiles/transport.png"/>
  </tile>
  <tile id="7">
-  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/upper class.png"/>
+  <image width="50" height="50" source="tiles/upper class.png"/>
  </tile>
  <tile id="8">
-  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/water.png"/>
+  <image width="50" height="50" source="tiles/water.png"/>
  </tile>
  <tile id="9">
-  <image width="50" height="50" source="../../../OneDrive/Pictures/tiles/outside map.png"/>
+  <image width="50" height="50" source="tiles/outside map.png"/>
  </tile>
 </tileset>
