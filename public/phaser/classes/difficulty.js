9a393d2deafec813b87c0552d15e784715a45a6e
Jim <jimmyxie48@gmail.com>
2020-04-29 22:14:06 -0400

    Added days that player will start getting more energy to presets

---
+++ public/phaser/classes/difficulty.js
@@ -0,0 +1,47 @@
+class Difficulty{
+    constructor(obj){
+        this.name = name;
+        this.infectivity = obj.infectivity;
+        this.severity = obj.severity;
+        this.lethality = obj.lethality;
+        this.cureProgressPerDay = obj.cureProgressPerDay;
+
+        //The i-th element of the list corresponds to the amount of energy the player will start to get on the i-th day
+        this.energyPerDay = obj.energyPerDay;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getInfectivity(){
+        return this.infectivity;
+    }
+
+    getSeverity(){
+        return this.severity;
+    }
+
+    getLethality(){
+        return this.lethality;
+    }
+
+    getDailyCureProgress(){
+        return this.cureProgressPerDay;
+    }
+
+    //Returns the amount of energy that the player should receive on this day
+    //If past the last day of the array, give the maximum amount of energy possible, 7
+    getEnergyToday(day){
+        var i = 0;
+        
+        while(i < this.energyPerDay.length){
+            if(this.energyPerDay[i] > day)
+                return i;
+            
+            i++;
+        }
+
+        return i;
+    }
+}
