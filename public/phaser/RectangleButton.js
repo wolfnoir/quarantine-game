fee069d4a0ae357898e763eab58862430987c0ab
wolfnoir <amber.yang@stonybrook.edu>
2020-05-16 19:03:44 -0400

    Added a thing that toggles global vs tile buttons

--- public/phaser/RectangleButton.js
+++ public/phaser/RectangleButton.js
@@ -7,4 +7,14 @@ class RectangleButton extends Phaser.GameObjects.Rectangle {
         this.buttonText = scene.add.text(x, y, text,
             { fontFamily: '"Courier New"', fontSize: '20px', fontWeight: 900, color: '0x000000' }).setOrigin(0.5);
     }
+
+    hideButton(){
+        this.buttonText.setAlpha(0);
+        super.setAlpha(0);
+    }
+
+    showButton(){
+        this.buttonText.setAlpha(1);
+        super.setAlpha(1);
+    }
 }
