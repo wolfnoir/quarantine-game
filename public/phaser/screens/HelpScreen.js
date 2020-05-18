022bc45af7cc4b7c96e78f386c942fd4592c5bb1
wolfnoir <amber.yang@stonybrook.edu>
2020-05-17 20:32:06 -0400

    Started adding extra info on the help page and added morale % to tile info

--- public/phaser/screens/HelpScreen.js
+++ public/phaser/screens/HelpScreen.js
@@ -45,7 +45,7 @@ class HelpScreen extends Phaser.Scene {
 		backButton.on('pointerdown', () => this.backClicked());
 
 		//page counter
-		this.pageCounter = this.add.text(this.game.config.width/2, this.game.config.height - 50, this.currentPage + "/10",
+		this.pageCounter = this.add.text(this.game.config.width/2, this.game.config.height - 50, this.currentPage + "/4",
 			{fontWeight: 'bold', fontSize: '24px'});
 
 		//next page button
@@ -72,7 +72,7 @@ class HelpScreen extends Phaser.Scene {
 			this.currentPage = this.currentPage + 1;
 		}
 		
-		this.pageCounter.setText(this.currentPage + "/10");
+		this.pageCounter.setText(this.currentPage + "/4");
 		this.textInfo.setText(this.updateText(this.currentPage));
 	}
 
@@ -85,8 +85,8 @@ class HelpScreen extends Phaser.Scene {
 			this.currentPage = this.currentPage - 1;
 		}
 		
-		this.pageCounter.setText(this.currentPage + "/10");
-		this.textInfo.setText(updateText(this.currentPage));
+		this.pageCounter.setText(this.currentPage + "/4");
+		this.textInfo.setText(this.updateText(this.currentPage));
 	}
 
 	updateText(num){
@@ -106,11 +106,56 @@ class HelpScreen extends Phaser.Scene {
 				"the whole city gets infected?\n\n\n" +
 				"Developed by Jeong Uk Lee,\nJimmy Xie, and Everett Yang";
 			case 2:
-				return "page 2";
+				return "------STARTING OUT------\n\n" +
+				"Your main goal is to fight against the\n" +
+				"pandemic until a cure can be found.\n" +
+				"Each day, you have several actions you\n" +
+				"can take to fight back against the\n" +
+				"infection. Different actions have\n" +
+				"different effects listed in their\n" +
+				"descriptions.\n\n" +
+				"INFECTIVITY - Affects the rate at\n" +
+				"which the disease spreads.\n\n" + 
+				"SEVERITY - Controls how badly the virus\n"+
+				"seems to be affecting others, thus\n" +
+				"affecting morale.\n\n" + 
+				"LETHALITY - Affects how quickly the\n" +
+				"virus kills those infected.\n\n" +
+				"RECOVERY - Affects the rate at which\n" +
+				"people recover from the disease.";
 			case 3:
-				return "page 3";
+				return "------ENERGY------\n\n" +
+				"Actions take time and resources, and\n"+
+				"thus each action has an associated\n" +
+				"energy cost. You are given a limited\n" +
+				"amount of energy each turn, so pick\n"+
+				"your moves wisely.\n\n" +
+				"------ACTIONS------\n\n" +
+				"There are two types of actions one\n" +
+				"can take each turn--global actions and\n" +
+				"tile actions. Global actions affect\n" +
+				"all the tiles on the map, but take\n"+
+				"more energy. Tile actions are specific\n"+
+				"to individual tiles.\n\n" +
+				"You may use tile actions by clicking\n" +
+				"on a tile and clicking on the actions\n" +
+				"that appear.";
 			case 4:
-				return "page 4";
+				return "------ACHIEVING VICTORY------\n\n" +
+				"Your only hope for defeating the\n" +
+				"pandemic will be to research a cure.\n" +
+				"However, the cure will be useless if\n" +
+				"the pandemic reaches catastrophic\n" +
+				"levels or if the people decide to riot.\n" +
+				"Thus, it is important to keep morale\n" +
+				"up and keep the threat level as low as\n"+
+				"possible.\n\n" +
+				"It's a race against the clock--with\n" +
+				"each turn, the virus becomes more and\n"+
+				"more vicious, causing the threat level\n" +
+				"to slowly climb. As this plague\n"+
+				"ravages the streets, the people become\n" +
+				"more and more restless.";
 		}
 	}
 
